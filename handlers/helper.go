18814e9f50f5457b920310549bf8a36769bdb43d
john.nnamchi <john.nnamchi@indexexchange.com>
2019-01-31 21:26:13 -0500

    Added documentation and clear modules

--- handlers/helper.go
+++ handlers/helper.go
@@ -2,34 +2,127 @@ package handlers
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+
+	"github.com/gorilla/schema"
 )
 
-func HandleSuccess(w *http.ResponseWriter, result interface{}) {
-	writer := *w
+// HTTP Response Header Codes
+const (
+	httpOK                  = 200
+	httpInternalServerError = 500
+)
 
-	marshalled, err := json.Marshal(result)
+// parseGETRequest is an example of how to read an incoming GET request and extract the
+// data in its url into a dedicated struct that can be used by other Go methods in the
+// project.
+func parseGETRequest(w http.ResponseWriter, r *http.Request) (DspRequest, error) {
 
+	// Initialize DspRequest struct we want to populate with data.
+	var dspRequest DspRequest
+
+	// Package gorilla/schema converts structs to and from form values.
+	// Here a decoder is instantiated to be able to read the form values
+	// of the incoming get request.
+	var decoder = schema.NewDecoder()
+
+	// The values in the request URL are extracted using Query() and are converted
+	// to form values.
+	values := r.URL.Query()
+
+	// The decoder can now insert the form values into the DspRequest struct.
+	err := decoder.Decode(&dspRequest, values)
 	if err != nil {
-		HandleError(w, 500, "Internal Server Error", "Error marshalling response JSON", err)
+		log.Printf("Error in GET parameters : %v", err)
+		return dspRequest, err
+	}
+
+	return dspRequest, nil
+}
+
+// sendPOSTRequest is an example of how to send a POST request to a server and receive
+// back a response.
+func sendPOSTRequest(dspRequest DspRequest, dspURL string) (*http.Response, error) {
+
+	// 1. Build the POST Request
+
+	// First we must get the data from the DspRequest struct into the body of an
+	// IXRTB POST request. We can do so using the net/url package.
+	postBody := url.Values{}
+	postBody.Set("s", dspRequest.S)
+	postBody.Set("l", dspRequest.L)
+	postBody.Set("d", dspRequest.D)
+	postBody.Set("a", strconv.Itoa(dspRequest.A))
+
+	// Instantiate a net/http client that can be used to make a http request to another server
+	client := &http.Client{}
+
+	// Build a new http request, which involves specifying the request type (POST), the request URL,
+	// and the data that will be included in the POST request's body.
+	dspRequestBody, _ := http.NewRequest("POST", dspURL, strings.NewReader(postBody.Encode()))
+	// Also specify headers to specify the format of the data being sent (Content-Type, in this case
+	// alphanumeric), and the length of the message.
+	dspRequestBody.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	dspRequestBody.Header.Add("Content-Length", strconv.Itoa(len(postBody.Encode())))
+
+	// 2. Send the POST Request
+	log.Printf("Sending request to DSP at: %v", dspURL)
+
+	return client.Do(dspRequestBody)
+}
+
+// parseJSONResponse is an example of how to parse a JSON response received from a server
+// that responded to the initial request.
+func parseJSONResponse(bid Bid, response *http.Response, dspURL string) (Bid, error) {
+
+	// Make sure to close the connection after this method completes (defer)
+	defer response.Body.Close()
+
+	// ioutil.ReadAll uses the response.Body reader to extract the data into a byte array (body).
+	// Check out this link for more about Golang's Reader and Writer interfaces:
+	// https://nathanleclaire.com/blog/2014/07/19/demystifying-golangs-io-dot-reader-and-io-dot-writer-interfaces/
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Printf("Error at response body from DSP at: %v", dspURL)
+		return bid, err
+	}
+
+	// Using the json library we can now extract the JSON data into a bid struct
+	// and populate its fields according to what was received in the response.
+	err = json.Unmarshal(body, &bid)
+	if err != nil {
+		log.Printf("Unable to unmarshal the json body of DSP at: %v", dspURL)
+		return bid, err
+	}
+
+	return bid, nil
+}
+
+// ReturnJSONResponse is an example of how to return a JSON formatted response to the entity
+// that sent the request. The JSON returned will be a JSON version of the struct that was
+// passed to it.
+func ReturnJSONResponse(w http.ResponseWriter, result interface{}) {
+
+	// The json Marshaller is excellent at converting Go structs into JSON objects. Simply
+	// provide the struct to convert and a JSON object with the struct's key/value pairs
+	// is created.
+	marshalled, err := json.Marshal(result)
+	if err != nil {
+		w.WriteHeader(httpInternalServerError)
+		w.Write([]byte("Internal Server Error"))
 		return
 	}
 
-	writer.Header().Set("Access-Control-Allow-Origin", "*")
-	writer.Header().Add("Content-Type", "application/json")
-	writer.WriteHeader(200)
-	writer.Write(marshalled)
-}
-
-func HandleError(w *http.ResponseWriter, code int, responseText string, logMessage string, err error) {
-	errorMessage := ""
-	writer := *w
-
-	if err != nil {
-		errorMessage = err.Error()
-	}
-	log.Printf("An error occure:", errorMessage)
-	writer.WriteHeader(code)
-	writer.Write([]byte(responseText))
+	// Using the response writer, set headers to allow cross-origin-resource-sharing and specify
+	// content returned is of type JSON. Provide the httpOk 200 status code and write the marshalled
+	// JSON object, which is how the response is returned.
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(httpOK)
+	w.Write(marshalled)
 }
--- handlers/ixrtb.go
+++ handlers/ixrtb.go
@@ -1,15 +1,8 @@
 package handlers
 
 import (
-	"encoding/json"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"net/url"
-	"strconv"
-	"strings"
-
-	"github.com/gorilla/schema"
 )
 
 type Bid struct {
@@ -36,7 +29,7 @@ var dspURLs = []string{
 	"http://127.0.0.1:9002/ixrtb",
 }
 
-// ServeAd is the ad-serving handler. It receives an IXRTB GET request, parses the values in the
+// RunAuction is the ad-serving handler. It receives an IXRTB GET request, parses the values in the
 // url fields, builds an IXRTB POST request to DSPs with those same values a the request body, and
 // sends a bid request to DSPs. It then waits for responses, selects the highest bidder and returns
 // the ad belonging to the Bid.
@@ -47,14 +40,18 @@ func RunAuction(w http.ResponseWriter, r *http.Request) {
 	// Parse the IXRTB GET request and put values into a DspRequest struct
 	dspRequest, parseError := parseGETRequest(w, r)
 	if parseError != nil {
-		HandleSuccess(&w, HeartBeatResponse{Okay: false})
+		ReturnJSONResponse(w, HeartBeatResponse{Okay: false})
 	}
 
-	var topBid Bid
-
+	// Instantiate a bidChannel and errChannel through which the bid responses will be passed
+	// through. This allows multiple requests to DSPs to be made concurrently, and their responses
+	// to be processed as they are received.
 	bidChannel := make(chan Bid)
 	errChannel := make(chan error)
 
+	// Loop over all the DSPs provided and send a bid request to each of them. A go routine for
+	// each DSP is necessary to ensure it is not waiting for the last DSP to respond before
+	// contacting another one.
 	for _, dspURL := range dspURLs {
 		go func(dspURL string) {
 			bid, err := getDSPBid(dspRequest, dspURL)
@@ -66,6 +63,7 @@ func RunAuction(w http.ResponseWriter, r *http.Request) {
 		}(dspURL)
 	}
 
+	var topBid Bid
 	for responsesReceived := 0; responsesReceived < len(dspURLs); {
 		select {
 		case gotBid := <-bidChannel:
@@ -79,62 +77,26 @@ func RunAuction(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	HandleSuccess(&w, topBid)
+	ReturnJSONResponse(w, topBid)
 }
 
-func parseGETRequest(w http.ResponseWriter, r *http.Request) (DspRequest, error) {
-
-	// Initialize DspRequest struct we want to populate
-	var dspRequest DspRequest
-
-	var decoder = schema.NewDecoder()
-	values := r.URL.Query()
-	err := decoder.Decode(&dspRequest, values)
-	if err != nil {
-		log.Printf("Error in GET parameters : %v", err)
-		return dspRequest, err
-	}
-
-	return dspRequest, nil
-}
-
+// getDSPBid is an example of how to build and send a POST request to send to a server.
+// It builds the POST request body data using values provided from a DspRequest struct,
+// instantiates a http client, and sends and receives the request using it.
 func getDSPBid(dspRequest DspRequest, dspURL string) (Bid, error) {
 
-	data := url.Values{}
-	data.Set("s", dspRequest.S)
-	data.Set("l", dspRequest.L)
-	data.Set("d", dspRequest.D)
-	data.Set("a", strconv.Itoa(dspRequest.A))
-
-	client := &http.Client{}
-
-	dspRequestBody, _ := http.NewRequest("POST", dspURL, strings.NewReader(data.Encode()))
-
-	dspRequestBody.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	dspRequestBody.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
-
-	log.Printf("Sending request to dsp:%v", dspURL)
-	response, err := client.Do(dspRequestBody)
-
+	// Specifiy the variable that will hold the final return result.
 	var bid Bid
 
+	// Using the client that was initialized before, invoke the Do method and provide it the
+	// POST request that was built. Notice how they are decoupled, so many clients can make
+	// the same POST request if they are provided with the same built request. This is also where
+	// we receive the response, and assign it to the response variable.
+	response, err := sendPOSTRequest(dspRequest, dspURL)
 	if err != nil {
-		log.Printf("Error at sending request to DSP:%v", dspURL)
+		log.Printf("Error at sending request to DSP at: %v", dspURL)
 		return bid, err
 	}
 
-	defer response.Body.Close()
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Printf("Error at response body from DSP:%v", dspURL)
-		return bid, err
-	}
-
-	err = json.Unmarshal(body, &bid)
-	if err != nil {
-		log.Printf("Unable to unmarshal the json body of DSP:%v", dspURL)
-		return bid, err
-	}
-
-	return bid, nil
+	return parseJSONResponse(bid, response, dspURL)
 }
